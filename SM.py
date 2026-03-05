1125c9da826fea658beae43f162af978037c5635
jayanib505 <bhulaja@wgc.school.nz>
2026-03-05 14:27:17 +1300

    Add files via upload

---
+++ SM.py
@@ -0,0 +1,30 @@
+# Build your plan using pseudocode
+
+# using comments , use your own words to describe HOW you might begin to build this.
+
+
+# Check with AI about your plan,
+
+# Then start writing code based on your comments
+
+
+# Make a 2D list with symbols for machine
+
+# Make a score variable and set to 0
+# Make a repeat symbol and set to true 
+
+# random pick symbols # spin
+
+# Print out 3 symbols in a row
+
+# check if they are the same
+
+# if yes add points to their score and print out a winning message
+
+# if else go to next set of code
+
+# ask user input if they want to spin again
+
+# if yes print a new shuffled set
+
+# if no end code and print final score/points won 
