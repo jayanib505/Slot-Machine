e24c669a83ca99499f691b0989b0b4c2407fc7f9
jayanib505 <bhulaja@wgc.school.nz>
2026-03-05 15:19:47 +1300

    Add files via upload

--- SM.py
+++ SM.py
@@ -8,23 +8,42 @@
 # Then start writing code based on your comments
 
 
+import random
+
 # Make a 2D list with symbols for machine
+symbols = ["🍒", "🎰", "$", "111", "333", "777"]
+row = []
 
-# Make a score variable and set to 0
-# Make a repeat symbol and set to true 
+# Play again
+play = True
 
-# random pick symbols # spin
+while play == True:
+    grid = []
+#Pick a random item from symbols and append it
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append(random.choice(symbols))
 
+        grid.append(row)
+ 
 # Print out 3 symbols in a row
+    for row in grid:
+        print(row)
 
-# check if they are the same
+# Check if symbols match
+    if row[0] == row[1] and row[1] == row[2]:
+        print("You won!")
 
-# if yes add points to their score and print out a winning message
 
-# if else go to next set of code
+# Ask user input if they want to spin again
+    spin_again = input("Would you like to spin again? y or n ".lower())
 
-# ask user input if they want to spin again
+    if spin_again == "y":
+        play = True
+        row.clear()
 
-# if yes print a new shuffled set
-
-# if no end code and print final score/points won 
+    else:
+        play = False
+        row.clear()
+    
