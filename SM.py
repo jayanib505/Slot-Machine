c3589676f19a8be327dfc97826ac72499ed7bfa8
jayanib505 <bhulaja@wgc.school.nz>
2026-04-02 14:39:06 +1300

    Add files via upload

--- SM.py
+++ SM.py
@@ -11,6 +11,9 @@ symbols = ["🍒", "🎰", "$", "111", "333", "777"]
 grid = []
 
 def generate_grid():
+    """
+    Append rows
+    """
     for i in range(3):
         current_row = []
         for j in range(3):
@@ -21,10 +24,16 @@ def generate_grid():
 
         
 def display_grid(grid):
+    """
+    Prints out grid
+    """
     for row in grid:
         print(f" | {' | '.join(row)} | ")
         
 def check_wins(grid):
+    """
+    Checking if symbols  in row match
+    """
     if grid[1][0] == grid[1][1] and grid[1][2] == grid[1][0] and grid[1][1] == grid[1][2]:
         print("YOU HAVE WON ROW 1!")
     if grid[0][0] == grid[0][1] and grid[0][2] == grid[0][0] and grid[0][1] == grid[0][2]:
@@ -32,9 +41,14 @@ def check_wins(grid):
     if grid[2][0] == grid[2][1] and grid[2][2] == grid[2][0] and grid[2][1] == grid[2][2]:
         print("YOU HAVE WON ROW 3!")
 
+#def repeat():
+  #  spin = input("Send y if you want to spin again?".lower())
+ #   if spin == "y":
+        
+        
 
 # main routine
-
-generate_grid()
-display_grid(grid)
-check_wins(grid)
+if __name__ == "__main__":
+    generate_grid()
+    display_grid(grid)
+    check_wins(grid)
