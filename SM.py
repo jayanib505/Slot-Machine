e1d4811935f2e578f8a6295e652d485cb096532e
jayanib505 <bhulaja@wgc.school.nz>
2026-05-06 10:24:56 +1200

    Add files via upload

--- SM.py
+++ SM.py
@@ -6,20 +6,24 @@
 
 import random
 
-# Make a 2D list with symbols for machine
-symbols = ["🍒", "🎰", "$", "111", "333", "777"]
-grid = []
 
 def generate_grid():
     """
     Append rows
     """
+    
+    # Runs code 3 times to create 3 rows
     for i in range(3):
+
+        # Empty row
         current_row = []
         for j in range(3):
+
+            # Generates random symbols and appends 3 into empty row
             symbol = random.choice(symbols)
             current_row.append(symbol)
         grid.append(current_row)
+    # Returns row to grid variable
     return grid
 
         
@@ -27,28 +31,52 @@ def display_grid(grid):
     """
     Prints out grid
     """
+
+     # Printing grid format
     for row in grid:
         print(f" | {' | '.join(row)} | ")
+
         
 def check_wins(grid):
     """
     Checking if symbols  in row match
     """
+
+    # Checks middle row for matches
     if grid[1][0] == grid[1][1] and grid[1][2] == grid[1][0] and grid[1][1] == grid[1][2]:
         print("YOU HAVE WON ROW 1!")
+
+
+    # Checks top row for matches
     if grid[0][0] == grid[0][1] and grid[0][2] == grid[0][0] and grid[0][1] == grid[0][2]:
         print("YOU HAVE WON ROW 2!")      
+
+    # Checks bottom row for matches
     if grid[2][0] == grid[2][1] and grid[2][2] == grid[2][0] and grid[2][1] == grid[2][2]:
         print("YOU HAVE WON ROW 3!")
+    
 
-#def repeat():
-  #  spin = input("Send y if you want to spin again?".lower())
- #   if spin == "y":
-        
-        
-
-# main routine
+# Main routine
 if __name__ == "__main__":
-    generate_grid()
-    display_grid(grid)
-    check_wins(grid)
+
+    # Symbol list 
+    symbols = ["🍒", "🎰", "$", "111", "333", "777"]
+    
+
+    # Loop until user quits
+    while True:
+        grid = []
+        spin = input("Do you want to spin the Slot Machine? (y/n)").lower()
+
+        # Ends Program
+        if spin == "n":
+            print("Goodbye!")
+            break
+        
+        # Get, display and check grid
+        generate_grid()
+        display_grid(grid)
+        check_wins(grid)
+
+    
+            
